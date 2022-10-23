08106b71b7e1f40725bf7efac319fbb99c763279
Aayush Jitendra Kumar <jitendra.kumar.epf@gmail.com>
2022-10-24 01:39:35 +0530

    code modified

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from functions import file_input, txt_input, txt_to_csv
 
-#Streamlit Web Application
+# Streamlit Web Application
 
 def main():
     with st.sidebar:
@@ -14,10 +14,10 @@ def main():
             file.seek(0)
             file_base_name = file.name.replace('.csv', '')
             df=file_input(file)
-            st.checkbox("Use container width", value=False, key="use_container_width")
+#             st.checkbox("Use container width", value=False, key="use_container_width")
             score="Sentiment Score : "+str(df.loc[number1-1,'score'])
             
-            st.dataframe(df.head(10), use_container_width=st.session_state.use_container_width)
+            st.dataframe(df.head(10))
             st.info(score)
             st.download_button("Download", df.to_csv(index=False),file_name='{0}.csv'.format(file_base_name))
         elif file.name[-3:] == 'txt' :
@@ -25,8 +25,8 @@ def main():
             file_base_name = file.name.replace('.txt', '')
             df=txt_to_csv(file)
             df2=txt_input(df)
-            st.checkbox("Use container width", value=False, key="use_container_width")
-            st.dataframe(df2.head(10), use_container_width=st.session_state.use_container_width)
+#             st.checkbox("Use container width", value=False, key="use_container_width")
+            st.dataframe(df2.head(10))
             score="Sentiment Score : "+str(df2.loc[number1-1,'score'])
             st.info(score)
             st.download_button("Download", df2.to_csv(index=False),file_name='{0}.csv'.format(file_base_name))
