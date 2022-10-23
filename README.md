6ae7f2514207f911d5ea607846033c6b93f7d88e
Aayush Jitendra Kumar <jitendra.kumar.epf@gmail.com>
2022-10-23 18:53:48 +0530

    code uploaded

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Emoji-Sentiment-Analysis-II
-NLP based application deployed using Streamlit
+
+# **Emoji-Sentiment-Analysis**
+## **Sentiment Analysis done using NlApi provided by expert.ai.**
+
+### Natural Language API undrestands texts in five languages leveraging the expert.ai Knowledge Graph to resolve ambiguous terms to their precise meaning. It's a public Cloud API: it doesn't need any IT infrastructure or installation, and it scales to your needs.
+### It gives you document analysis, comprising deep linguistic analysis—text subdivision, part-of-speech tagging, morphological analysis, lemmatization, syntactic analysis and semantic analysis—keyphrase extraction, named entity recognition, relation extraction and sentiment analysis.
+
+
---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from functions import file_input, txt_input, txt_to_csv
+
+#Streamlit Web Application
+
+def main():
+    with st.sidebar:
+        st.title("Input")
+        file = st.file_uploader("Load your file", type=["txt", "csv"])
+    with st.sidebar:
+        number1 = st.number_input('Index', min_value=0, step=1)
+    if file and number1 :
+        if file.name[-3:] == 'csv':
+            file.seek(0)
+            file_base_name = file.name.replace('.csv', '')
+            df=file_input(file)
+            st.checkbox("Use container width", value=False, key="use_container_width")
+            score="Sentiment Score : "+str(df.loc[number1-1,'score'])
+            
+            st.dataframe(df.head(10), use_container_width=st.session_state.use_container_width)
+            st.info(score)
+            st.download_button("Download", df.to_csv(index=False),file_name='{0}.csv'.format(file_base_name))
+        elif file.name[-3:] == 'txt' :
+            file.seek(0)
+            file_base_name = file.name.replace('.txt', '')
+            df=txt_to_csv(file)
+            df2=txt_input(df)
+            st.checkbox("Use container width", value=False, key="use_container_width")
+            st.dataframe(df2.head(10), use_container_width=st.session_state.use_container_width)
+            score="Sentiment Score : "+str(df2.loc[number1-1,'score'])
+            st.info(score)
+            st.download_button("Download", df2.to_csv(index=False),file_name='{0}.csv'.format(file_base_name))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
